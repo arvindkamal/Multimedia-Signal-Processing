d850fa89dee62cd6e65f30ed77b514390be03f6f
jiahong1996 <43502002+jiahong1996@users.noreply.github.com>
2018-09-25 01:05:34 -0500

    Add files via upload

--- Am_calculation.m
+++ Am_calculation.m
@@ -1,10 +1,10 @@
-function [RealAm, totalerror] = Am_calculation(max_p0,Sw)
+function [RealAm, totalerror, voiced_unvoiced] = Am_calculation(max_p0,Sw)
 
 w0 = 2*pi./max_p0;
 win = hamming(200); %w[n]
 winFFT = fft(win,1024);
 N =1024;
-columns = 298;
+columns = 267;
 
 for i= 1:columns
     am(1:floor(max_p0(i))-1,i) = ceil(((1:floor(max_p0(i))-1)-1/2).*w0(i).*N/(2*pi));
@@ -12,20 +12,37 @@ for i= 1:columns
 end
 
 
-for i= 1:columns
-    for j = 1:max_p0(i)-1        
-        top = Sw(am(j,i):bm(j,i),1)  .* conj(winFFT(am(j,i):bm(j,i),1));
-        bottom = abs(winFFT(am(j,i):bm(j,i),1)).^2;   
-        Am_voiced(j,i) = sum(top)./sum(bottom);
+
+
+for y= 1:columns
+    for j = 1:max_p0(y)-1
+        for x = am(j,y):bm(j,y)
+            
+            
+            top(1:bm(j,y)-am(j,y)+1,y) = Sw(x,y)  .* conj(winFFT(x,1));
+            
+            bottom(1:bm(j,y)-am(j,y)+1,y) = abs(winFFT(x,1)).^2; 
+        end
         
-        
-        %error1 = abs(framewinFFT(am(j,i):bm(j,i),1) - Am_voiced(j,i).* winFFT(am(j,i):bm(j,i),1)).^2;
-        error1 = abs(real(Sw(am(j,i):bm(j,i),1)) - real(Am_voiced(j,i)).* real(winFFT(am(j,i):bm(j,i),1)).^2);
-        error_voiced(j,i) = 1/(2*pi) *sum(error1);
-        
+    Am_voiced(j,y) = sum(top(:,y))/sum(bottom(:,y));   
     end    
+    
 end
 
+
+
+
+for i= 1:columns
+    for j = 1:max_p0(i)-1    
+        for x = am(j,i):bm(j,i)
+        
+        error1 = abs(Sw(x,1) - (Am_voiced(j,i)).*(winFFT(x,1))).^2;
+        end
+        
+        error_voiced(j,i) = 1/(2*pi) *sum(error1);
+    end
+    
+end
 
 
 
@@ -35,35 +52,56 @@ winFFTunvoiced= ones(1024,1);
 for i= 1:columns
     
     for j = 1:max_p0(i)-1
+        for x = am(j,i):bm(j,i)
         
-        top = Sw(am(j,i):bm(j,i),i)  .* conj(winFFTunvoiced(am(j,i):bm(j,i),1));
-        bottom = abs(winFFTunvoiced(am(j,i):bm(j,i),1)).^2;   
+            top = Sw(x,i)  .* conj(winFFTunvoiced(x,1));
+            bottom = abs(winFFTunvoiced(x,1)).^2;   
+        
+        end
         Am_unvoiced(j,i) = sum(top)./sum(bottom);
-        
-        
-        %error2 = abs(framewinFFT(am(j,i):bm(j,i),1) - Am_unvoiced(j,i).* winFFTunvoiced(am(j,i):bm(j,i),1)).^2;
-        error2 = abs(real(Sw(am(j,i):bm(j,i),1)) - real(Am_unvoiced(j,i)).* real(winFFTunvoiced(am(j,i):bm(j,i),1)).^2);
-        error_unvoiced(j,i) = 1/(2*pi) *sum(error2);
-        
-    end    
+    end  
+    
 end
 
 
-for i = 1:length(error_voiced(:,1))
-    for j = 1:298
+
+
+for i= 1:columns
+    
+    for j = 1:max_p0(i)-1
         
-        if (error_voiced(i,j)>error_unvoiced(i,j))
+        for x = am(j,i):bm(j,i)
+        error2 = abs(real(Sw(x,1)) - real(Am_unvoiced(j,i)).* real(winFFTunvoiced(x,1)).^2);
+        
+        end
+        
+        error_unvoiced(j,i) = 1/(2*pi) *sum(error2);
+    end
+    
+end
+
+
+
+
+
+
+
+for j = 1:length(error_voiced(:,1))
+    for i = 1:columns
+        
+        if (error_voiced(j,i)>error_unvoiced(j,i))
             
             
-            RealAm(i,j) = Am_unvoiced(i,j);
-            Realerror(i,j) = error_unvoiced(i,j);
-            
-            
+            RealAm(j,i) = Am_unvoiced(j,i);
+            Realerror(j,i) = error_unvoiced(j,i);
+            voiced_unvoiced(j,i)= 0;
+    
             
         else
             
-            RealAm(i,j) = Am_voiced(i,j);
-            Realerror(i,j) = error_voiced(i,j);
+            RealAm(j,i) = Am_voiced(j,i);
+            Realerror(j,i) = error_voiced(j,i);
+            voiced_unvoiced(j,i)= 1;
             
         end     
     end
@@ -71,9 +109,9 @@ end
 
 
 
-totalerror = zeros(1,298);
+totalerror = zeros(1,columns);
 
-for i = 1:298
+for i = 1:columns
     
     totalerror(i) = sum(Realerror(:,i));
     
--- Lab1_Jiahong_Chen.m
+++ Lab1_Jiahong_Chen.m
@@ -6,6 +6,8 @@
 
 [wavesound,fs] = audioread('s5.wav');
 %wavesound1=10000* wavesound;
+wavesound = wavesound(1:21504);
+%wavesound = wavesound+aver;
 
 %number of samples per frame
 Tframe = fs*0.025;
@@ -54,29 +56,29 @@ end
 win1=[zeros(399,1);win];
 
 %pad the frames with 399x298 of zeros
-frame1 = [zeros(399,298);frame(:,:)]; 
+frame1 = [zeros(399,columns);frame(:,:)]; 
 
 %pad the window function with 200 zeros then the window fun. then 199 more zeros
 win2 = [zeros(200,1);win;zeros(199,1)]; 
 
 %pad the frame with 200x298 zeros then the frames then 199x298 more zeros
-frame2 = [zeros(200,298);frame(:,:);zeros(199,298)];
+frame2 = [zeros(200,columns);frame(:,:);zeros(199,columns)];
 
 %preset zeros for temp1 in the for loop
 temp1 = zeros(200,1);
 
 %autocorrelation matrix initialization
-autocorr = zeros(399,298); %preset zeros for autocorrelation 
+autocorr = zeros(399,columns); %preset zeros for autocorrelation 
 
 for i = 1:columns
     for m = 0:398
         for n = 400:599
             
            %autocorrelation equation of w^2[n]s[n] (not summed up yet)          
-           temp1(n-399,1) =  win1(n).^2 .* frame1(n,i).* win2(n-m).^2 .* frame2(n-m,i); 
+           temp1(n-399,i) =  win1(n).^2 .* frame1(n,i).* win2(n-m).^2 .* frame2(n-m,i); 
         end
            %summing the autocorrelation arrays
-           autocorr(m+1,i) = sum(temp1);
+           autocorr(m+1,i) = sum(temp1(:,i));
            
            %%%%% autocorrelaton index in matlab starts from 1 to 399 but
            %%%%% matlab index(1) is -199 and index(399) is 199
@@ -87,42 +89,85 @@ end
 
 
 %phi(kp) matrix initialization
-phi_kp = zeros(40,1);
+
 
 %matrix initialization for p estimation from [20,90]
 p_sum_phi_kp = zeros(71,1);
 
 %arg max P matrix initialization
-max_p0=zeros(1,298);
+max_p0=zeros(1,columns);
+
+
+
+
 
 
 
 for i = 1:columns
     for p = 20:90
-        for k = 1:floor(398/p)
+        for k = 1:floor(199/p)
             
-            %phi(k*p) which is just autocorrelation index of k*p
-            phi_kp(k,1) = autocorr(k*p,i);
-        end
+            phi_kp(k,i) =  autocorr(200-k*p,i);
+            phi_kp(50+k,i) =   autocorr(200+k*p,i);
+                    
+            phi_kp(100,i) = autocorr(200,i);
+                        
+        end       
+        p_sum_phi_kp(p,i) = p*sum(phi_kp(:,i));     
         
-        %sum all phi(k*p) up and multiply it by p
-        p_sum_phi_kp(p-19,1) = p*sum(phi_kp);
     end
+    [maxp,tempIndex] = max(p_sum_phi_kp(:,i));
+    max_p0(i) = tempIndex;
     
-    %find the index of the largest P0 and +19 is for index correlation
-    [maxp,tempIndex] = max(p_sum_phi_kp);
-    max_p0(i) = tempIndex+19;
-
 end
 
 
-% to find the refined p, the original p is +- 2 with step of 0.2
+
+% w0 = 2*pi./max_p0;
+% 
+% 
+% for i= 1:columns
+%     am(1:floor(max_p0(i))-1,i) = ceil(((1:floor(max_p0(i))-1)-1/2).*w0(i).*N/(2*pi));
+%     bm(1:floor(max_p0(i))-1,i) = floor(((1:floor(max_p0(i))-1)+1/2).*w0(i).*N/(2*pi));
+% end
+% 
+% 
+% for y= 1:columns
+%     for j = 1:max_p0(y)-1
+%         for x = am(j,y):bm(j,y)
+%             
+%             
+%             top(1:bm(j,y)-am(j,y)+1,y) = framewinFFT(x,y)  .* conj(winFFT(x,1));
+%             
+%             bottom(1:bm(j,y)-am(j,y)+1,y) = abs(winFFT(x,1)).^2; 
+%         end
+%         
+%     Am_voiced(j,y) = sum(top(:,y))/sum(bottom(:,y));   
+%     end    
+%     
+% end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%to find the refined p, the original p is +- 2 with step of 0.2
 refined_len = length(-2:0.2:2);
 
 %p0 refined matrix initialization
 po_refined= zeros(refined_len,columns);
 
-%%for loop to generate the different p values
+%for loop to generate the different p values
 for i = 1: refined_len
     for j = 1: columns
     po_refined(i,j) = max_p0(j)-2+(i-1)*(0.2);
@@ -132,15 +177,20 @@ end
 
 totalerror_refined = zeros(refined_len,columns);
 
+
+%Use the Am_calculation function to use different p0 values 
 for i = 1:refined_len
     [RealAm, totalerror,z1] = Am_calculation(po_refined(i,:),framewinFFT);
-    totalerror_refined(i,:) = totalerror; 
-    
+    totalerror_refined(i,:) = totalerror;   
 end
-
+% % 
+% 
 Index=zeros(1,columns);
 Refined_p=zeros(1,columns);
 
+
+%find the p0 with the smallest error
+%Refined_p is the array with p0's that yield smallest error
 for i = 1:columns
 
     [Minp,P] = min(totalerror_refined(:,i));
@@ -150,122 +200,118 @@ for i = 1:columns
 end
 
 
+%use the refined p0 and run the Am_calculation function again
 [Refined_p_Am,Refined_p_error,z2] = Am_calculation(Refined_p(1,:),framewinFFT);
 
 
 
 
 
-for f= 0:columns-1;
-    for n = 80*f+1 : 80*(f+1)
-        for m= 1: floor(Refined_p(f+1))-1 
+
+%%%% synethesis part 
+
+
+for f= 1:columns;
+    for n = 80*(f-1)+1 : 80*(f)
+        for m= 1: floor(Refined_p(f))-1 
             
-            if f == columns-1
-               Am_syn_voiced(n) = real(Refined_p_Am(m,f+1));
-               w0_syn_voiced(n) = 2*pi/Refined_p(f+1);
+            if f == columns
+               Am_syn_voiced(n) = real(Refined_p_Am(m,f));
+               w0_syn_voiced(n) = 2*pi/Refined_p(f);
             else               
-               Am_syn_voiced(n) =  (f+1)-(n/Tskip) * real(Refined_p_Am(m,f+1)) + (n/Tskip-f)* real(Refined_p_Am(m,f+2));
-               w0_syn_voiced(n) =  (f+1)-(n/Tskip) * 2*pi/Refined_p(f+1) + (n/Tskip-f)* 2*pi/Refined_p(f+2);
-                
+               Am_syn_voiced(n) =  (f-((n-1)/Tskip)) * real(Refined_p_Am(m,f)) + ((n-1)/Tskip-(f-1))* real(Refined_p_Am(m,f+1));
+               w0_syn_voiced(n) =  (f-((n-1)/Tskip)) * 2*pi/Refined_p(f) + ((n-1)/Tskip-(f-1))* 2*pi/Refined_p(f+1);                
             end
             
             
-            if f==0 && n == 1                
+            if f==1  && n==1              
                 Theta_syn_voiced(n)= m*w0_syn_voiced(n);      
             else               
                 Theta_syn_voiced(n) = Theta_syn_voiced(n-1)+m*w0_syn_voiced(n);
-            end
-              
+            end  
             
             Am_mult_Cos_voiced(m) = Am_syn_voiced(n).* cos(Theta_syn_voiced(n));
-            
+               
         end
-        
-        
         Syn_voiced(n,1) = sum(Am_mult_Cos_voiced(m));
     end
 end
 
 
 
-
-
-
-
-
-% w0 = 2*pi./max_p0;
 % 
 % 
-% for i= 1:columns
-%     am(1:max_p0(i)-1,i) = ceil(((1:max_p0(i)-1)-1/2).*w0(i).*N/(2*pi));
-%     bm(1:max_p0(i)-1,i) = floor(((1:max_p0(i)-1)+1/2).*w0(i).*N/(2*pi));
-% end
-% 
-% %winFFT = fft(win,N); %fft of hamming window of length
-% %framewinFFT(1:N,i) = fft(framewin(1:Tframe,i),N); %Sw(w)
-% 
-% 
-% 
-% for i= 1:columns
-%     
-%     for j = 1:max_p0(i)-1
-%         
-%         top = framewinFFT(am(j,i):bm(j,i),1)  .* conj(winFFT(am(j,i):bm(j,i),1));
-%         bottom = abs(winFFT(am(j,i):bm(j,i),1)).^2;   
-%         Am_voiced(j,i) = sum(top)./sum(bottom);
-%         
-%         
-%         %error1 = abs(framewinFFT(am(j,i):bm(j,i),1) - Am_voiced(j,i).* winFFT(am(j,i):bm(j,i),1)).^2;
-%         error1 = abs(real(framewinFFT(am(j,i):bm(j,i),1)) - real(Am_voiced(j,i)).* real(winFFT(am(j,i):bm(j,i),1)).^2);
-%         error_voiced(j,i) = 1/(2*pi) *sum(error1);
-%         
-%         
-%     end    
-% end
-% 
-% 
-% 
-% 
-% winFFTunvoiced= ones(1024,1);
-% 
-% for i= 1:columns
-%     
-%     for j = 1:max_p0(i)-1
-%         
-%         top = framewinFFT(am(j,i):bm(j,i),i)  .* conj(winFFTunvoiced(am(j,i):bm(j,i),1));
-%         bottom = abs(winFFTunvoiced(am(j,i):bm(j,i),1)).^2;   
-%         Am_unvoiced(j,i) = sum(top)./sum(bottom);
-%         
-%         %error2 = abs(framewinFFT(am(j,i):bm(j,i),1) - Am_unvoiced(j,i).* winFFTunvoiced(am(j,i):bm(j,i),1)).^2;
-%         error2 = abs(real(framewinFFT(am(j,i):bm(j,i),1)) - real(Am_unvoiced(j,i)).* real(winFFTunvoiced(am(j,i):bm(j,i),1)).^2);
-%         error_unvoiced(j,i) = 1/(2*pi) *sum(error2);
-%         
-%     end    
-% end
-% 
-% for i = 1:84
-%     for j = 1:298
-%         
-%         if (error_voiced(i,j)>error_unvoiced(i,j))
-%             
-%             
-%             RealAm(i,j) = Am_unvoiced(i,j);
-%             Realerror(i,j) = error_unvoiced(i,j);
-%         else
-%             
-%             RealAm(i,j) = Am_voiced(i,j);
-%             Realerror(i,j) = error_voiced(i,j);
-%             
-%         end     
-%     end
-% end
-% 
-% 
-% 
-% for i = 1:298
-%     
-%     totalerror(i) = sum(Realerror(:,i));
-%     
-% end
-% 
-%     
+% % 
+% % % %%%Guassian Voice
+% % % var1 = normrnd(0, 0.01, 24000,1);
+% % % var2 = normrnd(0, 0.05, 24000,1);
+% % % var3 = normrnd(0, 0.10, 24000,1);
+% % % 
+% % noisewave1 = wavesound+var1;
+% % noisewave2 = wavesound+var2;
+% % noisewave3 = wavesound+var3;
+% % 
+% % for i = 1:columns
+% %     
+% %     
+% %     frame_noise1(1:Tframe,i) = wavesound(1+(Tskip*(i-1)):Tframe+(Tskip*(i-1)),1);
+% %     frame_noise2(1:Tframe,i) = wavesound(1+(Tskip*(i-1)):Tframe+(Tskip*(i-1)),1);
+% %     frame_noise3(1:Tframe,i) = wavesound(1+(Tskip*(i-1)):Tframe+(Tskip*(i-1)),1);
+% %    
+% %     framewin_noise1(1:Tframe,i) = frame(1:Tframe,i).*win;
+% %     framewin_noise2(1:Tframe,i) = frame(1:Tframe,i).*win;
+% %     framewin_noise3(1:Tframe,i) = frame(1:Tframe,i).*win;
+% %     
+% %     framewinFFT_noise1(1:N,i) = fft(framewin(1:Tframe,i),N); 
+% %     framewinFFT_noise2(1:N,i) = fft(framewin(1:Tframe,i),N); 
+% %     framewinFFT_noise3(1:N,i) = fft(framewin(1:Tframe,i),N); 
+% % end
+% % 
+% % for i = 1:columns
+% %     for m = 0:398
+% %         for n = 400:599
+% %             
+% %            %autocorrelation equation of w^2[n]s[n] (not summed up yet)          
+% %            temp_noise1(n-399,1) =  win1(n).^2 .* frame_noise1(n,i).* win2(n-m).^2 .* frame_noise1(n-m,i); 
+% %            temp_noise2(n-399,1) =  win1(n).^2 .* frame_noise2(n,i).* win2(n-m).^2 .* frame_noise2(n-m,i); 
+% %            temp_noise3(n-399,1) =  win1(n).^2 .* frame_noise3(n,i).* win2(n-m).^2 .* frame_noise3(n-m,i); 
+% %            
+% %         end
+% %            %summing the autocorrelation arrays
+% %            autocorr_noise1(m+1,i) = sum(temp_noise1);
+% %            autocorr_noise2(m+1,i) = sum(temp_noise2);
+% %            autocorr_noise3(m+1,i) = sum(temp_noise3);
+% %            
+% %            %%%%% autocorrelaton index in matlab starts from 1 to 399 but
+% %            %%%%% matlab index(1) is -199 and index(399) is 199
+% %     end
+% % end
+% % 
+% % for i = 1:columns
+% %     for p = 20:90
+% %         for k = 1:floor(398/p)
+% %             
+% %             %phi(k*p) which is just autocorrelation index of k*p
+% %             phi_kp_noise1(k,1) = autocorr_noise1(k*p,i);
+% %             phi_kp_noise2(k,1) = autocorr_noise2(k*p,i);
+% %             phi_kp_noise3(k,1) = autocorr_noise3(k*p,i);
+% %         end
+% %         
+% %         %sum all phi(k*p) up and multiply it by p
+% %         p_sum_phi_kp_noise1(p-19,1) = p*sum(phi_kp_noise1);
+% %         p_sum_phi_kp_noise2(p-19,1) = p*sum(phi_kp_noise2);
+% %         p_sum_phi_kp_noise3(p-19,1) = p*sum(phi_kp_noise3);
+% %     end
+% %     
+% %     %find the index of the largest P0 and +19 is for index correlation
+% %     [maxp_noise1,tempIndex_noise1] = max(p_sum_phi_kp_noise1);
+% %     [maxp_noise2,tempIndex_noise2] = max(p_sum_phi_kp_noise2);
+% %     [maxp_noise3,tempIndex_noise3] = max(p_sum_phi_kp_noise3);
+% %     
+% %     
+% %     max_p0_noise1(i) = tempIndex_noise1+19;
+% %     max_p0_noise2(i) = tempIndex_noise2+19;
+% %     max_p0_noise3(i) = tempIndex_noise3+19;
+% %     
+% % end
+% % 
